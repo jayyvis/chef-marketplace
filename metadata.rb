ea360825f9f302ee56c40162370807ef5258cb32
Mandeep <mandeep170@users.noreply.github.com>
2014-10-24 16:38:15 -0400

    update version number

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jellyfishopensource@bah.com'
 license          'All rights reserved'
 description      'Installs/Configures bah-marketplace'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 depends "apache2"
 depends "ntp"
