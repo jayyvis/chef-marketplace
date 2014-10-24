87c5e9647e3e0dce5104933c0b47a31a4aae1d52
Mandeep <mandeep170@users.noreply.github.com>
2014-10-24 16:37:36 -0400

    updating code_url
    updating code_url to pull from the latest master from github

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,4 +6,4 @@ default["chef-marketplace"]["db_prefix"] = ""
 
 default["chef-marketplace"]["cron_key"] = "iwlBW_YhoVc5daJjnhkCQsSNs-cLGop4U6j04bBEYJ8"
 
-default["chef-marketplace"]["code_url"] = "https://s3.amazonaws.com/dpi-releases/marketplace-latest.tar.gz"
+default["chef-marketplace"]["code_url"] = "https://github.com/booz-allen-hamilton/marketplace/archive/master.tar.gz"
